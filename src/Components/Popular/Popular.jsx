17710b9b086727b23b22616207f19c0854914157
Akash <101383490+Akashx1550@users.noreply.github.com>
2024-07-01 17:16:16 +0530

    Update Popular.jsx

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
-import Item from '../Item/Item'
-const Popular = () => {
+import React, { useEffect, useState } from 'react';
+import './Popular.css';
+import Item from '../Item/Item';
 
+const Popular = () => {
     const [popularProducts, setPopularProducts] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://trend-mart-backend.vercel.app/popularinwomen')
-        .then((res)=> res.json())
-        .then((data) => setPopularProducts(data));
-    },[])
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then((data) => setPopularProducts(data))
+            .catch((error) => {
+                console.error('Error fetching popular in women products:', error);
+            });
+    }, []);
 
     return (
         <div className='popular'>
             <h1>POPULAR IN WOMEN</h1>
             <hr />
             <div className="popular-item">
-                {popularProducts.map((item,i)=>{
-                    return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-                })}
+                {popularProducts.map((item, i) => (
+                    <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Popular
+export default Popular;
