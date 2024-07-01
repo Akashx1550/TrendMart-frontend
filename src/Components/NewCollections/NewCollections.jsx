82042fc757dd19096914eb4ea8ee54ec0c6b2da9
Akash <101383490+Akashx1550@users.noreply.github.com>
2024-07-01 17:16:38 +0530

    Update NewCollections.jsx

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,26 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollections.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import './NewCollections.css';
+import Item from '../Item/Item';
+
 const NewCollections = () => {
+    const [new_collection, setNew_collection] = useState([]);
 
-  const [new_collection, setNew_collection] = useState([]);
+    useEffect(() => {
+        fetch('https://trend-mart-backend.vercel.app/newcollections')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then((data) => setNew_collection(data))
+            .catch((error) => {
+                console.error('Error fetching new collections:', error);
+            });
+    }, []);
 
-  useEffect(()=>{
-    fetch('https://trend-mart-backend.vercel.app/newcollections')
-    .then((res)=> res.json())
-    .then((data)=> setNew_collection(data));
-  },[])
-  return (
-    <div className='new-collections'>
-        <h1>NEW COLLECTIONS</h1>
-        <hr />
-        <div className='collections'>
-            {new_collection.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            })}
+    return (
+        <div className='new-collections'>
+            <h1>NEW COLLECTIONS</h1>
+            <hr />
+            <div className='collections'>
+                {new_collection.map((item, i) => (
+                    <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+                ))}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default NewCollections
+export default NewCollections;
